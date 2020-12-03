1826700822f0bcbd5a3c8b091580f1ff62a2ed33
Donny Winston <dwinston@alum.mit.edu>
2020-12-03 11:42:41 -0500

    pip install stuff

--- setup.py
+++ setup.py
@@ -3,12 +3,22 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open("requirements/main.in") as fh:
+    install_requires = fh.read().splitlines()
+
+with open("requirements/dev.in") as fh:
+    extras_require_dev = fh.read().splitlines()
+
 setup(
     name="mongospawn",
     url="https://github.com/polyneme/mongospawn",
     packages=find_packages(),
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
+    install_requires=install_requires,
+    extras_require={
+        "dev": extras_require_dev,
+    },
     author="Donny Winston",
     author_email="donny@polyneme.xyz",
     description="Spawn MongoDB resources from JSON Schema",
